ee94491569f461f8bc8aa7b162ea456e0f569047
Turmunkh <turuuu@Turuu.local>
2022-05-11 09:06:38 +0800

    shppy

--- face/nuur.js
+++ face/nuur.js
@@ -1,51 +1,15 @@
-console.log("avaer");
-
-
-
 const logo = document.getElementById("logo");
-
 const huree = document.getElementById('huree');
-
 const enter =document.getElementById("enter");
-
 const input = document.querySelectorAll('input');
-
 const wrong = document.getElementById("wrong")
+const baraaniiToo =document.getElementById("zero")
+const zahialah =document.getElementById ("border")
 
-const border =document.getElementById("border")
-
-const zero =document.getElementById("zero")
-
-logo.addEventListener('click' , () =>{
-    huree.style.display = "block"
-
-    window.addEventListener("keydown" ,(e)=>{
-        if(e.keyCode === 13){
-
-
-            if(input[1].value === "hi"){
-                console.log("zov");
-            }
-            else{
-                wrong.style.display = "block"
-            }
-            if(input[2].value === "1234"){
-                console.log("zov");
-            }
-            else{
-                wrong.style.display = "block"
-            }
-        }
-    })
-    
+zahialah.addEventListener('click' , ()=>{
+    console.log("darlaa");
 });
 
-function nem(a , b ){
+function nemdeg (a , b){
     return a+b;
 }
-
-border.addEventListener("click" , ()=>{
-    nem(1 , zero.textContent );
-    zero.textContent +=  nem(1 , zero.textContent );
-
-})
---
+++ login/login.js
@@ -0,0 +1,15 @@
+document.getElementById('login').addEventListener("click" ,()=>{
+    const username = document.getElementsByTagName("input")[0].value;
+    const password = document.getElementsByTagName("input")[1].value;
+    if(username === "" || password === ""){
+        alert("Ta kod oo oruulna uu")
+    }
+    if(username === "turuu@gmail.com" || password === "1234"){
+        console.log("dd")
+        localStorage.setItem("login", username);
+        window.location.replace("");
+    }
+    else{
+        alert("Tanii oruulsan kod buruu baina")
+    }
+})
